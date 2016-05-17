d3e1b053d6f362da2be55f3ea3af11f0861ffe61
saurav1982 <saurav.note2@gmail.com>
2016-05-17 13:39:07 +0530

    Add files via upload

---
+++ CommonInterface.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyElevator
+{
+	interface CommonInterface
+	{
+	}
+
+    interface IStop
+    {
+        void StayPut(int floor);
+    }
+
+    public interface ILogger
+    {
+        void Register(string error);
+    }
+
+    public interface IMessage
+    {
+        void ShowMessage(string message);
+    }
+}
